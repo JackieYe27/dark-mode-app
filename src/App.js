28f206698b6069340840c7c75be0846a51e04319
Jackie Ye <jackieye27@gmail.com>
2021-08-16 22:10:16 -0700

    add toggle and local storage

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
-import React, { useState, useEffect } from 'react'
-import data from './data'
-import Article from './Article'
+import React, { useState, useEffect } from 'react';
+import data from './data';
+import Article from './Article';
+
+const getStorageTheme = () => {
+  let theme = "light-theme";
+  if (localStorage.getItem("theme")) {
+    theme = localStorage.getItem("theme");
+  }
+  return theme;
+}
 
 function App() {
+  const [theme, setTheme] = useState(getStorageTheme());
+
+  useEffect(() => {
+    document.documentElement.className = theme;
+    localStorage.setItem("theme", theme);
+  },[theme]);
+
+  const toggleTheme = () => {
+    if (theme === "light-theme") {
+      setTheme("dark-theme");
+    } else {
+      setTheme("light-theme");
+    }
+  }
+
   return <main>
     <nav>
       <div className="nav-center">
         <h1>Overreacted</h1>
-        <button className="btn">Toggle</button>
+        <button className="btn" onClick={toggleTheme}>Toggle</button>
       </div>
     </nav>
     <section className="articles">
